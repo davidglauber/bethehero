4e68953ce833133df2618869f6251cd6bda4c4ad
ifal2017 <davidglauber2010@hotmail.com>
2020-03-24 13:39:00 -0300

    Ajeitando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+
+  const [contador, setContador] = useState(0)
+
+  alert('contador: ' + contador)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Iae RCROOKS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => setContador(contador + 1)}
         >
           Learn React
-        </a>
+        </button>
       </header>
     </div>
   );
