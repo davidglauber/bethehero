4abe435f15d9c437a2aa81bf6825922cf9ed3e23
ifal2017 <davidglauber2010@hotmail.com>
2020-03-25 15:06:26 -0300

    Front end mudanças

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css'
+import React from 'react';
+import Routes from './routes/routes';
+import './global.css';
 
 function App() {
 
-  const [contador, setContador] = useState(0)
-
-  alert('contador: ' + contador)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Iae RCROOKS
-        </p>
-        <button
-          onClick={() => setContador(contador + 1)}
-        >
-          Learn React
-        </button>
-      </header>
-    </div>
+    <Routes/>
   );
 }
 
